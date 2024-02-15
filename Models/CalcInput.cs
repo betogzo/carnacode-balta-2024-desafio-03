140feec7842afe5b76e0c5ef49a28e90d949a9d7
betogzo <albertogaleazzo@msn.com>
2024-02-14 21:07:08 -0300

    replace gender string to enum and add required field validations

--- Models/CalcInput.cs
+++ Models/CalcInput.cs
@@ -1,13 +1,19 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Imc.Models.Enums;
 
 namespace Imc.Models;
 
 public class CalcInput
 {
-    [Range(5, 500, ErrorMessage = "O peso deve ser um valor entre 5 e 500")]
-    public float? Peso { get; set; }
+    private const string RequiredErrorMessage = "O campo {0} é obrigatório.";
+    
+    [Required(ErrorMessage = RequiredErrorMessage)]
     [Range(1, 2.5, ErrorMessage = "A altura deve ser um valor entre 1 e 2.5")]
     public float? Altura { get; set; }
-    [RegularExpression("^[mMfF]$", ErrorMessage = "Apenas 'M' ou 'F' são permitidos no campo Sexo.")]
-    public string Sexo { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = RequiredErrorMessage)]
+    [Range(5, 500, ErrorMessage = "O peso deve ser um valor entre 5 e 500")]
+    public float? Peso { get; set; }
+    
+    public ESexo Sexo { get; set; }
 }
